99ac81955ec85336d5a774961976eb5f8c0d05a6
Kevin Gilpin <kevin.gilpin@gmail.com>
2020-03-16 14:03:02 -0700

    Remove listeners with ComponentWillUnmount

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -18,13 +18,21 @@ class Channels extends React.Component {
         this.addListeners();
     }
 
+    componentWillUnmount() {
+        this.removeListeners();
+    }
+
     addListeners = () => {
         let loadedChannels = [];
         this.state.channelsRef.on('child_added', snap => {
             loadedChannels.push(snap.val());
             this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
         });
-    }
+    };
+
+    removeListeners = () => {
+        this.state.channelsRef.off();
+    };
 
     setFirstChannel = () => {
         const firstChannel = this.state.channels[0];
