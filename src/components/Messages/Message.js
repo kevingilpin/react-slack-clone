8ea2bda664c31e0495aed59acdc1334361bb82ba
Kevin Gilpin <kevin.gilpin@gmail.com>
2020-03-18 11:46:09 -0700

    Bug Fix: Remove duplicate messages from displaying

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -18,7 +18,6 @@ const Message = ({ message, user }) => (
         <Comment.Content className={isOwnMessage(message, user)}>
             <Comment.Author as="a">{message.user.name}</Comment.Author>
             <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-            <Comment.Text>{message.content}</Comment.Text>
             {isImage(message) 
                 ? <Image src={message.image} className="message__image" />
                 : <Comment.Text>{message.content}</Comment.Text>
