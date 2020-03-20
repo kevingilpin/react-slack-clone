b20b786ab5762c4d78b4ad424b71d1bde25a1d96
Kevin Gilpin <kevin.gilpin@gmail.com>
2020-03-20 14:17:51 -0700

    Display Channel Info in MetaPanel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Segment, Accordion, Header, Icon, AccordionPanel } from 'semantic-ui-react';
+import { Segment, Accordion, Header, Icon, Image } from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
     state = {
         activeIndex: 0,
-        privateChannel: this.props.isPrivateChannel
+        privateChannel: this.props.isPrivateChannel,
+        channel: this.props.currentChannel
     };
 
     setActiveIndex = (event, titleProps) => {
@@ -15,14 +16,14 @@ class MetaPanel extends React.Component {
     };
 
     render() {
-        const { activeIndex, privateChannel } = this.state;
+        const { activeIndex, privateChannel, channel } = this.state;
 
         if (privateChannel) return null;
-        
+
         return (
-            <Segment>
+            <Segment loading={!channel}>
                 <Header as="h3" attached="top">
-                    About # Channel
+                    About # {channel && channel.name}
                 </Header>
                 <Accordion styled attached="true">
                     <Accordion.Title
@@ -35,7 +36,7 @@ class MetaPanel extends React.Component {
                         Channel Details
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 0}>
-                        details
+                        {channel && channel.details}
                     </Accordion.Content>
 
                     <Accordion.Title
@@ -61,7 +62,10 @@ class MetaPanel extends React.Component {
                         Created By
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 2}>
-                        creator details
+                        <Header as="h3">
+                            <Image circular src={channel && channel.createdBy.avatar} />
+                            {channel && channel.createdBy.name}
+                        </Header> 
                     </Accordion.Content>
                 </Accordion>
             </Segment>
