9e2096cf0740366c5e38f03b0302c391b30d37f9
Kevin Gilpin <kevin.gilpin@gmail.com>
2020-03-21 15:55:41 -0700

    Add Typing Listeners to see when other users are typing

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -22,7 +22,10 @@ class Messages extends React.Component {
     searchterm: '',
     searchLoading: false,
     searchResults: [],
-    isChannelStarred: false
+    isChannelStarred: false,
+    typingRef: firebase.database().ref('typing'),
+    typingUsers: [],
+    connectedRef: firebase.database().ref('.info/connected')
   };
 
   componentDidMount() {
@@ -36,6 +39,7 @@ class Messages extends React.Component {
 
   addListeners = channelId => {
     this.addMessageListener(channelId);
+    this.addTypingListeners(channelId);
   };
 
   addMessageListener = channelId => {
@@ -51,6 +55,41 @@ class Messages extends React.Component {
       this.countUserPosts(loadedMessages);
     });
   };
+
+  addTypingListeners = channelId => {
+    let typingUsers = [];
+    this.state.typingRef.child(channelId).on('child_added', snap=> {
+      if (snap.key !== this.state.user.uid) {
+        typingUsers = typingUsers.concat({
+          id: snap.key,
+          name: snap.val()
+        });
+        this.setState({ typingUsers });
+      }
+    });
+
+    this.state.typingRef.child(channelId).on('child_removed', snap => {
+      const index = typingUsers.findIndex(user => user.id === snap.key);
+      if (index !== -1) {
+        typingUsers = typingUsers.filter(user => user.id !== snap.key);
+        this.setState({ typingUsers });
+      }
+    })
+
+    this.state.connectedRef.on('value', snap => {
+      if (snap.val() === true) {
+        this.state.typingRef
+          .child(channelId)
+          .child(this.state.user.uid)
+          .onDisconnect()
+          .remove(err => {
+            if (err !== null) {
+              console.error(err);
+            }
+          })
+      }
+    })
+  }
 
   addUserStarsListener = (channelId, userId) => {
     this.state.usersRef
@@ -170,8 +209,16 @@ class Messages extends React.Component {
     return channel ? `${this.state.privateChannel ? '@' : '#'}${channel.name}` : '';
   };
 
+  displayTypingUsers = users => (
+    users.length > 0 && users.map(user => (
+      <div style={{ display: "flex", alignItems: "center", marginBottom: ".2em" }} key={user.id}>
+        <span className="user__typing">{user.name} is typing</span> <Typing />
+      </div>
+    ))
+  );
+
   render() {
-    const { messagesRef, messages, channel, user, progressBar, numUniqueUsers, searchTerm, searchResults, searchLoading, privateChannel, isChannelStarred } = this.state;
+    const { messagesRef, messages, channel, user, progressBar, numUniqueUsers, searchTerm, searchResults, searchLoading, privateChannel, isChannelStarred, typingUsers } = this.state;
 
     return (
       <React.Fragment>
@@ -188,9 +235,7 @@ class Messages extends React.Component {
         <Segment>
           <Comment.Group className={progressBar ? 'messages__progress' : 'messages'}>
             {searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <span className="user__typing">kevin is typing</span> <Typing />
-            </div>
+            {this.displayTypingUsers(typingUsers)}
           </Comment.Group>
         </Segment>
 
